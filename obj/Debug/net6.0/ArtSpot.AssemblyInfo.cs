f9d54cacbbdc53d16fc942d63766b09b208a1753
Vikashkgupta <v43400089@gmail.com>
2024-04-18 21:39:24 +0530

    update in vikash 18/04/2024

--- obj/Debug/net6.0/ArtSpot.AssemblyInfo.cs
+++ obj/Debug/net6.0/ArtSpot.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArtSpot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3c1bb1348e9d95f542fdb617aaf399586beb46e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArtSpot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArtSpot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
